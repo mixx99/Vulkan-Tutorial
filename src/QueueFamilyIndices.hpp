231f8be7dc50d509cf5c4ea27bd5af33ec5d64b2
mixx99 <maxkriv1@gmail.com>
2025-04-14 18:16:43 +0300

    QueueFamilyIndices updated

--- src/QueueFamilyIndices.hpp
+++ src/QueueFamilyIndices.hpp
@@ -3,4 +3,9 @@
 
 struct QueueFamilyIndices {
   std::optional<uint32_t> graphicsFamily;
+  std::optional<uint32_t> presentFamily;
+
+  bool isComplete() {
+    return graphicsFamily.has_value() && presentFamily.has_value();
+  }
 };
