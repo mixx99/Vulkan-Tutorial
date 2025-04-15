81aecc65fd3247be7fc774d0790eff4b3b6d4808
mixx99 <maxkriv1@gmail.com>
2025-04-15 18:21:15 +0300

    bug fixed

--- src/device.cpp
+++ src/device.cpp
@@ -80,8 +80,7 @@ QueueFamilyIndices Device::findQueueFamilies(VkPhysicalDevice device) {
 
   return indices;
 }
-Device::SwapChainSupportDetails
-Device::querySwapChainSupport(VkPhysicalDevice device) {
+Device::SwapChainSupportDetails Device::querySwapChainSupport(VkPhysicalDevice device) {
   SwapChainSupportDetails details;
   vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface,
                                             &details.capabilities);
@@ -92,13 +91,12 @@ Device::querySwapChainSupport(VkPhysicalDevice device) {
     vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount,
                                          details.formats.data());
   }
-  uint32_t formatCount;
-  vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
-
-  if (formatCount != 0) {
-    details.formats.resize(formatCount);
-    vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount,
-                                         details.formats.data());
+  uint32_t presentModeCount;
+  vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+  
+  if (presentModeCount != 0) {
+      details.presentModes.resize(presentModeCount);
+      vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
   }
   return details;
 }
