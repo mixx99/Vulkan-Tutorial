7079cfeb8df30a4bd0c557ccabbf5585dce42782
mixx99 <maxkriv1@gmail.com>
2025-04-16 18:20:50 +0300

    created graphics pipeline

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -26,6 +26,45 @@ void Triangle::initVulkan() {
   device.createLogicalDevice();
   device.createSwapChain();
   createImageViews();
+  createGraphicsPipeline();
+}
+
+void Triangle::createGraphicsPipeline() {
+  auto vertShaderCode = readFile("src/shaders/vert.spv");
+  auto fragShaderCode = readFile("src/shaders/frag.spv");
+  VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+  VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+  VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+  vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+  vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+  vertShaderStageInfo.module = vertShaderModule;
+  vertShaderStageInfo.pName = "main";
+#ifdef NDEBUG
+  std::cout << "VertShaderCode = " <<vertShaderCode.size() << " Baits." << std::endl;
+  std::cout << "FragShaderCode = " << fragShaderCode.size() << " Baits." << std::endl;
+#endif
+  VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+  fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+  fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+  fragShaderStageInfo.module = fragShaderModule;
+  fragShaderStageInfo.pName = "main";
+
+  VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+  vkDestroyShaderModule(device.getDevice(), fragShaderModule, nullptr);
+  vkDestroyShaderModule(device.getDevice(), vertShaderModule, nullptr);
+}
+
+VkShaderModule Triangle::createShaderModule(const std::vector<char>& code) {
+  VkShaderModuleCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+  createInfo.codeSize = code.size();
+  createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+  VkShaderModule shaderModule;
+  if (vkCreateShaderModule(device.getDevice(), &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create shader module!");
+  }
+  return shaderModule;
 }
 
 void Triangle::createImageViews(){
--- src/triangle.hpp
+++ src/triangle.hpp
@@ -8,6 +8,8 @@
 // std
 #include <optional>
 #include <vector>
+#include <fstream>
+#include <iostream>
 
 namespace vtt // vulkan triangle tutorial
 {
@@ -37,5 +39,21 @@ private:
   void createInstance();
   bool checkValidationLayerSupport();
   void createImageViews();
+  void createGraphicsPipeline();
+  VkShaderModule createShaderModule(const std::vector<char>& code);
+  static std::vector<char> readFile(const std::string& filename) {
+    std::cout << filename << std::endl;
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+    if (!file.is_open()) {
+      throw std::runtime_error("failed to open file!");
+    }
+    size_t fileSize = (size_t) file.tellg();
+    std::vector<char> buffer(fileSize);
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+    file.close();
+  
+    return buffer;
+  }
 };
 } // namespace vtt
